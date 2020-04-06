27dc7d0b34564e81ad38ab0bf427517d7dd1021e
ksukhaldun <kseniya.khaldun@gmail.com>
2020-04-06 14:34:13 +0300

    Updates

---
+++ precache-manifest.e950b4735ecec3e0b482ab1f0690be4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8f64b80c1a784fb7ca3435ba7b753b",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "06e62772fefbcbd413a2",
+    "url": "/giphy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9bd346c8d8620839de5e",
+    "url": "/giphy/static/js/2.45a7253e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphy/static/js/2.45a7253e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e62772fefbcbd413a2",
+    "url": "/giphy/static/js/main.a9e5c6da.chunk.js"
+  },
+  {
+    "revision": "a4a842084921ae464513",
+    "url": "/giphy/static/js/runtime-main.e4b9ccbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/giphy/static/media/logo.5d5d9eef.svg"
+  }
+]);
